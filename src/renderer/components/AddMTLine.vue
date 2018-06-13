29e8b511a08347ebf48a83d512f3ca4910ce23ce
Shian <shianyip@gmail.com>
2018-06-14 02:40:34 +0800

    feat: add Image viewer to AddMTLine component

--- src/renderer/components/AddMTLine.vue
+++ src/renderer/components/AddMTLine.vue
@@ -10,19 +10,29 @@
         </el-header>
         <el-main style="background-color: #e4e4e4">
           <div style="background: white; padding: 10px;">
+            <el-dialog
+              title="Image Viewer"
+              :visible.sync="dialogVisible"
+            >
+              <l-mul-canvas
+               buffer="mt_img_buffer"
+               header-name="MT Image">
+              </l-mul-canvas>
+              <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="dialogVisible = false">
+                  Save
+                </el-button>
+              </span>
+            </el-dialog>
             <el-row>
               <el-col :span="4">
                 <block-tag tag-name="Image"></block-tag>
                 <el-card :body-style="{ padding: '0px' }">
                   <img src="../assets/mt.png" class="image">
                   <div style="padding: 14px;">
-                    <span>Meta Data</span>
                     <div class="bottom clearfix">
-                      <time class="time">{{ currentFileName }}</time>
-                      <br/>
-                      <br/>
-                      <el-button type="text" class="button">View Frames
-                      </el-button>
+
                     </div>
                   </div>
                 </el-card>
@@ -34,10 +44,12 @@
                 <block-tag tag-name="Operation Space"></block-tag>
                 <el-card class="box-card">
                   <div slot="header" class="clearfix">
-                    <span>Actions</span>
+                    <span>Image Viewer</span>
                     <el-tooltip content="Done">
-                      <el-button style="float: right; padding: 5px;" circle
-                                 icon="el-icon-check" type="success">
+                      <el-button
+                      @click="dialogVisible = true"
+                      style="float: right; padding: 5px;" circle
+                                 icon="el-icon-search" type="success">
                       </el-button>
                     </el-tooltip>
                   </div>
@@ -71,20 +83,20 @@
   import LSide from './basic/LSide';
   import LFooter from './basic/LFooter';
   import BlockTag from './basic/BlockTag';
+  import LMulCanvas from './basic/LMulCanvas.vue';
 
   export default {
     data() {
       return {
-        currentFileUrl: '',
-        currentFileName: '_Pig Tub 12 uM 10% Alexa647 label_200 nM EB1_50 nM ' +
-        'chTOG-RFP_laser488 10%_laser561 10%_laser640 7%_exposuretime 100ms_' +
-        'intervaltime 1s_35C--3_',
         layersValue: (130 / this.layersNum) * 100,
         layersNum: 1000,
+        dialogVisible: false,
       };
     },
     components: {
-      LHeader, LSide, LFooter, BlockTag,
+      LHeader, LSide, LFooter, BlockTag, LMulCanvas,
+    },
+    methods: {
     },
   };
 </script>
